74c6297181c8550ffdc3e771fd685639e7d6eff4
Husnain-Nadeem <mhusnainscoper123@gmail.com>
2026-09-07 15:58:48 +0500

    Day4-env

---
+++ Day-04/venv.py
@@ -0,0 +1,22 @@
+# Python Workspaces
+
+# Python workspaces refer to the environment in which you develop and run your Python code. They include the Python interpreter, installed libraries, and the current working directory. Understanding workspaces is essential for managing dependencies and code organization.
+
+# Python workspaces can be local or virtual environments. A local environment is the system-wide Python installation, while a virtual environment is an isolated environment for a specific project. You can create virtual environments using tools like `virtualenv` or `venv`.
+
+# **Example:**
+
+# ```bash
+pip install virtualenv
+# # Create a virtual environment
+ python -m venv projectname
+
+# # Activate the virtual environment (on Windows)
+# projectname\Scripts\activate
+
+# # Activate the virtual environment (on macOS/Linux)
+ source projectname/bin/activate
+# ```
+pip list | grep jira
+
+# Once activated, you work in an isolated workspace with its Python interpreter and library dependencies.

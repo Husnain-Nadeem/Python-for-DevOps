af4ce779ae69e47317f05c8b7a471d47e767e023
Husnain-Nadeem <mhusnainscoper123@gmail.com>
2026-09-07 15:05:43 +0500

    Day4-module

---
+++ Day-04/function.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    c= a + b
+    return c
+def subtract(a,b):
+    c= a - b
+    return c
+def multiply(a,b):
+    c= a * b
+    return c
+def divide(a,b):
+    c= a / b
+    return c
+
+print ("Addition is:", add(10,20))
+print ("Subtraction is:", subtract(20,10))
+print ("Multiplication is:", multiply(10,20))
+print ("Division is:", divide(20,10))
---
+++ Day-04/module_better.py
@@ -0,0 +1,5 @@
+import function as mb
+print("Addition by using module  is:", mb.add(5, 6))
+print("Subtraction by using module  is:", mb.subtract(20, 10))
+print("Multiplication by using module  is:", mb.multiply(10, 20))
+print("Division by using module  is:", mb.divide(20, 10))

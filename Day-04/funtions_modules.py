aab11c67b73e3fec2710bce8e1c9a2df8d8b7edf
Husnain-Nadeem <mhusnainscoper123@gmail.com>
2026-09-07 14:53:40 +0500

    Day4-functions

---
+++ Day-04/funtions_modules.py
@@ -0,0 +1,25 @@
+def add():
+    a = 10
+    b = 20
+    c = a + b
+    print("Addition is:", c)
+def subtract():
+    a = 20
+    b = 10
+    c = a - b
+    print("Subtraction is:", c)
+def multiply():
+    a = 10
+    b = 20
+    c = a * b
+    print("Multiplication is:", c)
+def divide():
+    a = 20
+    b = 10
+    c = a / b
+    print("Division is:", c)
+
+add()
+subtract()
+multiply()
+divide()

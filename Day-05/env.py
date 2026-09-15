2f9d8f8cb4593b86eb98c13dea6991a8738d35b3
Husnain-Nadeem <mhusnainscoper123@gmail.com>
2026-09-15 23:35:35 +0500

    Env

--- Day-05/env.py
+++ Day-05/env.py
@@ -4,3 +4,6 @@ print(os.getenv("HOME"))
 print(os.getenv("password"))
 print(os.getenv("pass"))
 print(os.getenv("apitoken"))
+
+#command to set environment variable in terminal
+# export password=1234
